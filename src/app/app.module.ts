10ca08bb9d7bae22c590a96e93fb750934fcf22a
Juanes <juanesgarciag@gmail.com>
2022-09-11 02:07:22 -0500

    Trabajando @Input, interfaces, modularizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.modules';
 import { ContadorModule } from './contadores/contador.modules';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { ContadorModule } from './contadores/contador.modules';
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
