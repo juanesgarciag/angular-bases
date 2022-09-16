6926105e450f1b379b358d13f5f4a437054ee4e9
Juanes <juanesgarciag@gmail.com>
2022-09-15 23:41:45 -0500

    Fin de la sesión 5, servicios, formModule, ngModel, @Input, @Output y eventEmitter

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -8,26 +9,11 @@ import { Personaje } from '../interfaces/dbz.interface';
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
-
   nuevo: Personaje ={
     nombre: '',
     poder: 0
   }
 
-  agregarNuevoPersonaje(arg: Personaje){
-    this.personajes.push(arg);
-  }
-
-
+  constructor() {}
 
 }
